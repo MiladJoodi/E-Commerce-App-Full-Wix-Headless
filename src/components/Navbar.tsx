6845f273e2f93c6245801e188b65a3f6db99bea4
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 00:04:38 +0330

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -15,10 +16,14 @@ const Navbar = () => {
       {/* Bigger Screen */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* left */}
-        <div></div>
+        <div className="w-1/3">
+          <Link href="/">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+          </Link>
+        </div>
 
         {/* Right */}
-        <div></div>
+        <div className="w-2/3"></div>
       </div>
     </div>
   );
