2b8486dd7e75deb1644d0465df46e39407d64bd4
Milad Joodi <winbeta.com@gmail.com>
2024-07-11 16:56:50 +0330

    up

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,7 +18,7 @@ const Menu = () => {
         onClick={() => setOpen((prev) => !prev)}
       />
       {open && (
-        <div className="">
+        <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8">
           <Link href="/">Homepage</Link>
           <Link href="/">Shop</Link>
           <Link href="/">Deals</Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 import Menu from "./Menu";
 
 const Navbar = () => {
-    return (
-        <div className="h-20 px-4 md:px-8 lg:px-16 2xl:px-64">
-            
-            {/* Mobile */}
-            <Link href="/">LAMA</Link>
-            <Menu />
-
-        </div>
-    );
-}
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 2xl:px-64 bg-red-100 relative">
+      <div className="h-full flex items-center justify-between bg-blue-100">
+        {/* Mobile */}
+        <Link href="/">
+        <div className="text-2xl tracking-wide">LAMA</div>
+        </Link>
+        <Menu />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
