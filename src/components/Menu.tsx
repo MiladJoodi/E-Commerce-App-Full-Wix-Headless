4fe407c380cbe4199d1a049311cd4784cfb45e47
Milad Joodi <winbeta.com@gmail.com>
2024-07-11 15:47:47 +0330

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const Menu = () => {
@@ -16,6 +17,17 @@ const Menu = () => {
         className="cursor-pointer"
         onClick={() => setOpen((prev) => !prev)}
       />
+      {open && (
+        <div className="">
+          <Link href="/">Homepage</Link>
+          <Link href="/">Shop</Link>
+          <Link href="/">Deals</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">Logout</Link>
+          <Link href="/">Cart (1)</Link>
+        </div>
+      )}
     </div>
   );
 };
