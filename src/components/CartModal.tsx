dc8a225b7813c8f046cacf25c4fd54348e693dfb
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 14:45:40 +0330

    up

---
+++ src/components/CartModal.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const CartModal = () => {
+    return (
+        <div>
+            CartModal
+        </div>
+    );
+}
+
+export default CartModal;
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,14 +1,66 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import CartModal from "./CartModal";
 
 const NavIcons = () => {
-    return (
-        <div className="flex items-center gap-4 xl:gap-6">
-            <Image src="/profile.png" alt="" width={22} height={22} className="cursor-pointer" />
-            <Image src="/notification.png" alt="" width={22} height={22} className="cursor-pointer" />
-            <Image src="/cart.png" alt="" width={22} height={22} className="cursor-pointer" />
+  // state for open cart and profile dropdown
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  //   router
+  const router = useRouter();
+
+  //   TEMPORARY
+  const isLoggedIn = false;
+
+  //   Open Profile Menu
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+        router.push("/login")
+    }
+    setIsProfileOpen(prev => !prev)
+  };
+
+  return (
+    <div className="flex items-center gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/">Profile</Link>
+          <div className="mt-2 cursor-pointer">Logout</div>
         </div>
-    );
-}
+      )}
+      <Image
+        src="/notification.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <Image
+        src="/cart.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+        onClick={()=>setIsCartOpen(prev=>!prev)}
+      />
+      {isCartOpen && (
+        <CartModal />
+      )}
+    </div>
+  );
+};
 
 export default NavIcons;
