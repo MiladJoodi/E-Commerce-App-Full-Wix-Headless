58eba703c43cfc5c3274d6728f979fd15df5db94
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 04:10:36 +0330

    up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ import NavIcons from "./NavIcons";
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 2xl:px-64 bg-red-100 relative">
+    <div className="h-20 px-4 md:px-8 lg:px-16 2xl:px-64relative">
       <div className="h-full flex items-center justify-between md:hidden">
         {/* Mobile */}
-        <Link href="/" className="flex items-center gap-3">
+        <Link href="/">
           <div className="text-2xl tracking-wide">LAMA</div>
         </Link>
         <Menu />
@@ -19,7 +19,7 @@ const Navbar = () => {
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* left */}
         <div className="w-1/3">
-          <Link href="/">
+          <Link href="/"  className="flex items-center gap-3">
             <Image src="/logo.png" alt="" width={24} height={24} />
             <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+
 const SearchBar = () => {
     return (
-        <div>
-            SearchBar
-        </div>
+        <form className="flex justify-between ">
+            <input type="text" placeholder="Search" />
+            <button className="cursor-pointer">
+                <Image src="/search.png" alt="" width={16} height={16} />
+            </button>
+        </form>
     );
 }
 
