1a635acb83e8437ffe7dee8e778adb53e0a44517
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 15:32:06 +0330

    up

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,9 +1,18 @@
 "use client"
 
 const CartModal = () => {
+
+    const cartItems = false;
+
     return (
         <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
-            CartModal
+            {!cartItems ? (
+                <div className="">Cart is Empty</div>
+            ): (
+                <div className="">
+
+                </div>
+            )}
         </div>
     );
 }
