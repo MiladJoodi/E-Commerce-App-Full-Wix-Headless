3cb8d76218aa0ee216840705e2b88aaad0e60644
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 14:17:49 +0330

    navbar

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 
 
 const NavIcons = () => {
     return (
-        <div>
-            NavIcons
+        <div className="flex items-center gap-4 xl:gap-6">
+            <Image src="/profile.png" alt="" width={22} height={22} className="cursor-pointer" />
+            <Image src="/notification.png" alt="" width={22} height={22} className="cursor-pointer" />
+            <Image src="/cart.png" alt="" width={22} height={22} className="cursor-pointer" />
         </div>
     );
 }
