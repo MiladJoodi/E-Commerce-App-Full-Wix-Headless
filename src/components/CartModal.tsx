3735c23f08cc454f2e09be23ac7742bc2297a784
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 15:24:39 +0330

    update

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -2,7 +2,7 @@
 
 const CartModal = () => {
     return (
-        <div>
+        <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
             CartModal
         </div>
     );
