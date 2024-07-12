bad5b2427f20c12ce572d1fd2c06226926cf2e63
Milad Joodi <winbeta.com@gmail.com>
2024-07-12 15:20:43 +0330

    up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,15 +18,24 @@ const Navbar = () => {
       {/* Bigger Screen */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* left */}
-        <div className="w-1/3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/"  className="flex items-center gap-3">
             <Image src="/logo.png" alt="" width={24} height={24} />
             <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
+
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
+
         </div>
 
         {/* Right */}
-        <div className="w-2/3 flex items-center justify-between gap-8">
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
         <SearchBar />
         <NavIcons />
         </div>
