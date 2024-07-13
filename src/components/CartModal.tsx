9ad171d240b3e5beb0b941a90e6d2f08290fc537
Milad Joodi <winbeta.com@gmail.com>
2024-07-13 14:24:34 +0330

    up

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -6,34 +6,69 @@ const CartModal = () => {
   const cartItems = true;
 
   return (
-    <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {!cartItems ? (
         <div className="">Cart is Empty</div>
       ) : (
-        <div className="">
-          <Image
-            src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={72}
-            height={96}
-            className="object-cover rounded-md"
-          />
-          <div className="">
-            {/* TOP */}
-            <div>
-              {/* TITLE */}
-              <div>
-                <h3>Product Name</h3>
-                <div className="">$49</div>
-              </div>
-              {/* DESC */}
+        <div className="flex flex-col gap-8">
+          {/* ITEM */}
+          <div className="flex gap-4">
+            <Image
+              src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={72}
+              height={96}
+              className="object-cover rounded-md"
+            />
+            <div className="">
+              {/* TOP */}
               <div className="">
-                available
+                {/* TITLE */}
+                <div className="flex items-center justify-between gap-8">
+                  <h3 className="font-semibold">Product Name</h3>
+                  <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                </div>
+                {/* DESC */}
+                <div className="">available</div>
+              </div>
+
+              {/* BOTTOM */}
+              <div className="flex justify-between text-sm">
+                <span className="text-gray-500">Qty. 2</span>
+                <span className="text-blue-500">Remove</span>
               </div>
             </div>
-
-            {/* BOTTOM */}
           </div>
+
+          {/* ITEM */}
+          <div className="flex gap-4">
+            <Image
+              src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={72}
+              height={96}
+              className="object-cover rounded-md"
+            />
+            <div className="">
+              {/* TOP */}
+              <div className="">
+                {/* TITLE */}
+                <div className="flex items-center justify-between gap-8">
+                  <h3 className="font-semibold">Product Name</h3>
+                  <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                </div>
+                {/* DESC */}
+                <div className="">available</div>
+              </div>
+
+              {/* BOTTOM */}
+              <div className="flex justify-between text-sm">
+                <span className="text-gray-500">Qty. 2</span>
+                <span className="text-blue-500">Remove</span>
+              </div>
+            </div>
+          </div>
+          
         </div>
       )}
     </div>
