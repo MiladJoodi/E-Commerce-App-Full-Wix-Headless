3082ca3647448ace533e910e5efd965801fd811a
Milad Joodi <winbeta.com@gmail.com>
2024-07-13 22:25:00 +0330

    slider and dots

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -41,7 +41,7 @@ const Slider = () => {
         {slides.map((slide) => (
           <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
             {/* TEXT CONTAINER */}
-            <div className="h-1/2 xl:w-1/2 flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
               <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
               <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
               <Link href={slide.url}>
@@ -49,12 +49,24 @@ const Slider = () => {
               </Link>
             </div>
             {/* IMG CONTAINER */}
-            <div className="h-1/2 xl:w-1/2 relative">
+            <div className="h-1/2 xl:w-1/2 xl:h-full relative">
               <Image src={slide.img} alt="" fill sizes="100%" className="object-cover" />
             </div>
           </div>
         ))}
       </div>
+      
+      {/* DOTS */}
+        <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+          {slides.map((slide, index)=> (
+            <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${current === index ? "scale-150" : ""}`} key={slide.id}>
+              {current === index && (
+                <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+              )}
+            </div>
+          ))}
+        </div>
+
     </div>
   );
 };
