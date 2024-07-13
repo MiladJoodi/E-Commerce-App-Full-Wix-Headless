be587690f7a0c5df70f80c380b79d293e27dc202
Milad Joodi <winbeta.com@gmail.com>
2024-07-13 22:09:07 +0330

    Update Slider.tsx

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -41,11 +41,11 @@ const Slider = () => {
         {slides.map((slide) => (
           <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
             {/* TEXT CONTAINER */}
-            <div className="h-1/2 xl:w-1/2">
-              <h2>{slide.description}</h2>
-              <h1>{slide.title}</h1>
+            <div className="h-1/2 xl:w-1/2 flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
               <Link href={slide.url}>
-                <button>SHOP NOW</button>
+                <button className="rounded-md bg-black text-white py-3 px-4">SHOP NOW</button>
               </Link>
             </div>
             {/* IMG CONTAINER */}
