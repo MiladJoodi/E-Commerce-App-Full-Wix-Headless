aa76c4d6e73f9256f476df59fcc87ebbc8137bb6
Milad Joodi <winbeta.com@gmail.com>
2024-07-13 14:41:31 +0330

    Update CartModal.tsx

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -10,66 +10,53 @@ const CartModal = () => {
       {!cartItems ? (
         <div className="">Cart is Empty</div>
       ) : (
-        <div className="flex flex-col gap-8">
-          {/* ITEM */}
-          <div className="flex gap-4">
-            <Image
-              src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt=""
-              width={72}
-              height={96}
-              className="object-cover rounded-md"
-            />
-            <div className="">
-              {/* TOP */}
+        <>
+          {/* LIST */}
+          <div className="flex flex-col gap-8">
+            {/* ITEM */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
               <div className="">
-                {/* TITLE */}
-                <div className="flex items-center justify-between gap-8">
-                  <h3 className="font-semibold">Product Name</h3>
-                  <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                  </div>
+                  {/* DESC */}
+                  <div className="">available</div>
                 </div>
-                {/* DESC */}
-                <div className="">available</div>
-              </div>
 
-              {/* BOTTOM */}
-              <div className="flex justify-between text-sm">
-                <span className="text-gray-500">Qty. 2</span>
-                <span className="text-blue-500">Remove</span>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
               </div>
             </div>
           </div>
-
-          {/* ITEM */}
-          <div className="flex gap-4">
-            <Image
-              src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt=""
-              width={72}
-              height={96}
-              className="object-cover rounded-md"
-            />
-            <div className="">
-              {/* TOP */}
-              <div className="">
-                {/* TITLE */}
-                <div className="flex items-center justify-between gap-8">
-                  <h3 className="font-semibold">Product Name</h3>
-                  <div className="p-1 bg-gray-50 rounded-sm">$49</div>
-                </div>
-                {/* DESC */}
-                <div className="">available</div>
-              </div>
-
-              {/* BOTTOM */}
-              <div className="flex justify-between text-sm">
-                <span className="text-gray-500">Qty. 2</span>
-                <span className="text-blue-500">Remove</span>
-              </div>
+          {/* BOTTOM */}
+          <div className="">
+            <div className="flex items-center justify-between font-semibold">
+              <span>Subtotal</span>
+              <span>$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Shipping and taxes calculated at checkout.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+              <button className="">Checkout</button>
             </div>
           </div>
-          
-        </div>
+        </>
       )}
     </div>
   );
