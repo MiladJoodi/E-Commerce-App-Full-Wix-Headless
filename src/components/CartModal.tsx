007c8b329677ed06ba9e7ac97d35ac3917de7db9
Milad Joodi <winbeta.com@gmail.com>
2024-07-13 14:52:40 +0330

    Update CartModal.tsx

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -11,6 +11,7 @@ const CartModal = () => {
         <div className="">Cart is Empty</div>
       ) : (
         <>
+        <h2 className="text-xl">Shopping Cart</h2>
           {/* LIST */}
           <div className="flex flex-col gap-8">
             {/* ITEM */}
@@ -41,6 +42,36 @@ const CartModal = () => {
                 </div>
               </div>
             </div>
+
+            {/* ITEM */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/26341034/pexels-photo-26341034/free-photo-of-a-view-of-a-canyon-with-a-river-running-through-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                  </div>
+                  {/* DESC */}
+                  <div className="">available</div>
+                </div>
+
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+
           </div>
           {/* BOTTOM */}
           <div className="">
@@ -53,7 +84,7 @@ const CartModal = () => {
             </p>
             <div className="flex justify-between text-sm">
               <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
-              <button className="">Checkout</button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">Checkout</button>
             </div>
           </div>
         </>
